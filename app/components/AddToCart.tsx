6ccec51932e3b14bfa26aefdd3afa39d5060678b
5n0weirdo <snoweirdo@protonmail.com>
2023-11-03 09:31:06 +0200

    Fetched todo data

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -1,10 +1,15 @@
-"use client"
+"use client";
 import React from "react";
 
 const AddToCart = () => {
   return (
     <div>
-      <button className="px-3 py-3 rounded-xl text-slate-100 dark:text-slate-100 bg-slate-900 dark:bg-slate-100" onClick={() => console.log("Clicked")}>Add To Cart</button>
+      <button
+        className="px-3 py-3 rounded-xl text-slate-100 dark:text-slate-100 bg-slate-900 dark:bg-slate-100"
+        onClick={() => console.log("Clicked")}
+      >
+        Add To Cart
+      </button>
     </div>
   );
 };
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import React from "react";
 
-const UserPage = () => {
+interface User {
+  id: number;
+  name: string;
+}
+
+const UserPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
   return (
     <div>
       <Link href="/">
@@ -10,6 +18,13 @@ const UserPage = () => {
         </button>
       </Link>
       <h1 className="text-2xl text-slate-900 font-semibold">User Page</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <h1>{user.name}</h1>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
